01cc7342fe7701ed7a5538ef934d862f7398a126
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2022-05-19 19:13:32 +0600

    complete main hero section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import MainBanner from '../../assets/images/main-banner.png';
+import SolonaLogo from '../../assets/images/solana-logo.png';
+
+function Banner() {
+    return (
+        <div className="">
+            <div className="px-4 sm:px-0 grid grid-cols-1 md:grid-cols-8">
+                <div className="   md:col-span-5 flex flex-col justify-center gap-y-3">
+                    <p className="text-3xl md:text-4xl lg:text-5xl ">Music for NFT creators.</p>
+                    <p className="text-2xl md:text-3xl lg:text-4xl -mt-2">
+                        Digital Music for fans.
+                    </p>
+                    <p className=" text-sm font-thin mt-3">
+                        Music NFTs will continue to revolutionize the way that artists and fans
+                        create community together as we enter the upcoming year — undoubtedly
+                        changing the trajectory of countless budding music careers.{' '}
+                    </p>
+                    <div className="flex">
+                        <div className="flex flex-col">
+                            <p className="text-xs font-thin">Powered by</p>
+                            <img className="w-24 h-3" src={SolonaLogo} alt="SOLONA" />
+                        </div>
+                        <button className="ml-9 flex justify-center items-center border border-white py-2 px-5 rounded-2xl font-thin hover:bg-blue-700 transition duration-500 ">
+                            <div>Discover More</div>
+                            <div className="ml-2">&gt;</div>{' '}
+                        </button>
+                    </div>
+                </div>
+                <div className="   md:col-span-3 lg:-mt-12 ">
+                    <img className="" src={MainBanner} alt="mainBanner" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Banner from '../components/Banner/Banner';
 import Footer from '../shared/sharedcomponents/Footer';
 import Navbar from '../shared/sharedcomponents/Navbar';
 import Styles from '../styles/Home.module.css';
@@ -8,22 +9,11 @@ function Home() {
         <>
             <div className={` ${Styles.bgImage1}`}>
                 <div className="backdrop-blur-xl bg-white/10   ">
-                    <Navbar className="" />
+                    <Navbar />
                     <div className=" border-b main-container " />
                 </div>
-                <div className="py-96 ">
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tenetur
-                        ullam laboriosam ipsum, consectetur animi facere cumque tempora cupiditate
-                        quo iure voluptatum excepturi veniam totam incidunt dolore doloribus quidem
-                        laudantium laborum ex cum aperiam. Inventore, perferendis recusandae? Eum
-                        sed in possimus quis temporibus quaerat aliquam reiciendis blanditiis dolore
-                        esse corrupti fuga autem nisi harum adipisci, ut quia nesciunt assumenda
-                        doloremque ipsam! Amet eaque nam, deleniti unde iure est fugit natus
-                        blanditiis quia ab esse, porro rerum odit earum neque! Possimus tempora ab
-                        reiciendis blanditiis adipisci accusamus commodi explicabo, excepturi eos
-                        perspiciatis neque nulla magnam. Saepe natus accusamus ut ullam nihil!
-                    </p>
+                <div className="main-container">
+                    <Banner />
                 </div>
             </div>
             <Footer />
