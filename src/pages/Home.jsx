218f77ed693b56ccd25645ee8e70240789c03753
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2022-05-20 01:24:12 +0600

    Added new component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
+import BannerTwo from '../components/BannerTwo/BannerTwo';
+import FanBar from '../components/FanBar/FanBar';
 import Footer from '../shared/sharedcomponents/Footer';
 import Navbar from '../shared/sharedcomponents/Navbar';
 import Styles from '../styles/Home.module.css';
 
 function Home() {
     return (
-        <>
-            <div className={` ${Styles.bgImage1}`}>
-                <div className="backdrop-blur-xl bg-white/10   ">
-                    <Navbar />
-                    <div className=" border-b main-container " />
-                </div>
-                <div className="main-container">
-                    <Banner />
-                </div>
+        <div className={` ${Styles.bgImage1}`}>
+            <div className="backdrop-blur-xl bg-white/10   ">
+                <Navbar />
+                <div className=" border-b main-container " />
             </div>
+            <div className="main-container">
+                <Banner />
+            </div>
+            <FanBar />
+            <BannerTwo />
             <Footer />
-        </>
+        </div>
     );
 }
 
