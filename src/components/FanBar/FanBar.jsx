8799052ed389a374600c132515b2b80ac0b0ccb3
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2022-05-20 03:00:12 +0600

    add margin

--- src/components/FanBar/FanBar.jsx
+++ src/components/FanBar/FanBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FanBar() {
     return (
-        <div className="py-3 my-10 lg:my-0">
+        <div className="py-3 lg:mt-20  ">
             <div className=" border-b border-white/50  " />
             <div className="main-container flex gap-x-3 justify-around py-1">
                 <p> 🧊 New way to reach the new fan</p>
